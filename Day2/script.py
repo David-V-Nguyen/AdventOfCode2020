fffe7256472b316cc725cb2c8ea0c8d95daa6c95
David Nguyen <dave_n998@hotmail.com>
2020-12-21 22:19:02 +0000

    Completed Regex formatting and completed Part 2 of challenge

--- Day2/script.py
+++ Day2/script.py
@@ -6,45 +6,54 @@ f = open('Day2/input.txt')
 # read the data in f and assign it to variable passwords
 words = f.readlines()
 
+#print(words)
+
 # remove the spaces and '\n' from the data
+#passwords = list(map(str.strip, words))
+
 passwords = list(map(str.strip, words))
 
-print(passwords)
+#print(passwords)
 
 # Sort the rules and passwords for each item in collection using dictionary??
 
 # Separate rules from passwords
-rules = []
-for i in passwords: # i = password
-    rules.append(re.split('r[1-20]\s\w:', i))
 
-print(rules)
+correct_passwords = []
+for i in passwords: # i = single password
+    #rules.append(re.split('[1-9][0-9]?', i))
+    match = re.match(r"([1-9][0-9]?)\-([1-9][0-9]?) ([a-z]): ([a-z]+)", i) 
+    min_, max_, character, password = match.groups() # tuple above is unpacked
+    min_ = int(min_)
+    max_ = int(max_)
+    c = password.count(character)
+    if min_ <= c <= max_: # '=>' this is an operator, some weird shit
+        correct_passwords.append(password)
+    print(c)
 
-# Convert rules from string => integer
-numRules = []
-for x in rules:
-    numRules.append(int(x))
+print(len(correct_passwords))
 
-print(numRules)
+# 1 - 3 = either position 1 and 2 must have the character to be correct
+# 
+# Find the number of correct passwords 
+valid_count = 0
+for i in passwords: # i = single password
+    #rules.append(re.split('[1-9][0-9]?', i))
+    match = re.match(r"([1-9][0-9]?)\-([1-9][0-9]?) ([a-z]): ([a-z]+)", i) 
+    pos_a, pos_b, character, password = match.groups() # tuple above is unpacked
+    print(match.groups())
+    pos_a = int(pos_a)
+    pos_b = int(pos_b)
+    occurrences = 0
+    #index_a = pos_a - 1 
+    if password[pos_a-1] == character:
+        occurrences +=1
 
-# Separate password phrase from rules
-'''
-passPhrase = []
-for p in passwords:
-    passPhrase.append(re.split('r[a-z]', p))
+    if password[pos_b-1] == character:
+        occurrences +=1  
 
-print(passPhrase)
-'''
+    if occurrences == 1:
+        valid_count +=1
 
-# Create a dictionary for the rules and passwords
-
-
-
-# Check if the passwords match the rules stated
-
-# If password is true to rules then store entry in variable 'correctPasswords'
-correctPasswords = []
-
-
-# Find and Display number of total correct passwords 
+print(valid_count)
 
