b60c7007fc6e6d51da5532cb8ae299fcfdcb2b2e
David Nguyen <dave_n998@hotmail.com>
2020-12-21 19:58:52 +0000

    Currently working on regex for rules

--- Day2/script.py
+++ Day2/script.py
@@ -6,12 +6,45 @@ f = open('Day2/input.txt')
 # read the data in f and assign it to variable passwords
 words = f.readlines()
 
-print(words)
-
 # remove the spaces and '\n' from the data
 passwords = list(map(str.strip, words))
 
 print(passwords)
 
-# 
-rules = re.split('', passwords)
+# Sort the rules and passwords for each item in collection using dictionary??
+
+# Separate rules from passwords
+rules = []
+for i in passwords: # i = password
+    rules.append(re.split('r[1-20]\s\w:', i))
+
+print(rules)
+
+# Convert rules from string => integer
+numRules = []
+for x in rules:
+    numRules.append(int(x))
+
+print(numRules)
+
+# Separate password phrase from rules
+'''
+passPhrase = []
+for p in passwords:
+    passPhrase.append(re.split('r[a-z]', p))
+
+print(passPhrase)
+'''
+
+# Create a dictionary for the rules and passwords
+
+
+
+# Check if the passwords match the rules stated
+
+# If password is true to rules then store entry in variable 'correctPasswords'
+correctPasswords = []
+
+
+# Find and Display number of total correct passwords 
+
