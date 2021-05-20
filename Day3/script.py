294345a3e12d9c316d896c66fcc93c700a0695ae
David Nguyen <dave_n998@hotmail.com>
2021-05-20 16:56:45 +0100

    some testing for github token access

--- Day3/script.py
+++ Day3/script.py
@@ -1,1 +1,14 @@
-# 
+f = open('Day3/input.txt')
+
+landscape = f.readlines()
+
+# Clear spaces and \n characters
+ground = list(map(str.strip, landscape)) 
+
+print(ground) 
+
+def main():
+    for i in ground:
+        
+
+# some testing for github tokens 
